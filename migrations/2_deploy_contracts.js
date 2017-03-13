67a4c72cff0d69cb1a4ef37f606ec8bcdbdc1907
Alex Bash <avbashlykov@gmail.com>
2017-03-14 01:19:58 +0300

    token as separated contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,26 @@
 var TokenFund = artifacts.require("./TokenFund.sol");
+var Fund = artifacts.require("./Fund.sol");
 
 module.exports = function(deployer, network) {
 	if (network == "testnet") {
 	  	var founder = "0x74c128191f01dce550d81b19785b54b191259d2d";
 	  	var multisig = "0x74c128191f01dce550d81b19785b54b191259d2d";
 	  	var support = "0x42ccb9b37dd47dec2bbf85d01b0202ca237e109d";
-	  	deployer.deploy(TokenFund, founder, multisig, support);
 	} else if (network == 'development') {
+		var founder = web3.eth.accounts[0];
+		var multisig = web3.eth.accounts[0];
 		var support = "0x42ccb9b37dd47dec2bbf85d01b0202ca237e109d";
-		deployer.deploy(TokenFund, web3.eth.accounts[0], web3.eth.accounts[0], support);
 	}
+
+	deployer.deploy(TokenFund, founder).then(function() {
+  		return deployer.deploy(Fund, founder, multisig, support, TokenFund.address);
+  	}).then(function() {
+  		return TokenFund.deployed();
+  	}).then(function(instance) {
+  		if (founder != web3.eth.accounts[0]) {
+  			// emission contract address must be set manualy
+  			return;
+  		}
+  		return instance.changeEmissionContractAddress(Fund.address);
+  	});
 };
