1da42ce6a377b9e70071ca148631c747e2da6aab
Alex Bash <avbashlykov@gmail.com>
2017-03-24 11:19:04 +0300

    migration changed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,8 @@ var Fund = artifacts.require("./Fund.sol");
 
 module.exports = function(deployer, network) {
 	var acc = web3.eth.accounts[0];
-	var ethAddress = "0xd7461f8e7c47f7ba122e68669b563ec0f8328910";
+	var ethAddress = web3.eth.accounts[0];
+
 	if (network == "testnet") {
 	  	var founder = "0x05aec595f8cd12d794aeac63c0988d5d7e247442";
 	  	var multisig = "0x6E8Ec8a086B485940B435D18303b59b6C35048C1";
@@ -13,10 +14,13 @@ module.exports = function(deployer, network) {
 		var multisig = acc;
 		var support = "0x42ccb9b37dd47dec2bbf85d01b0202ca237e109d";
 	} else if (network == 'live') {
-    var founder = "0x212de331b2a8c21fcf091c8f3cd13e613bb0af95";
-    var multisig = "0xBA3b826539161A4c3BF681752021847c25A2B46a";
-    // EtherionLab multisig address.
-    var support = "0x9e8fAb94CADA52a584Ec6F10042c3Bb165E79114";
+		var founder = "0x212de331b2a8c21fcf091c8f3cd13e613bb0af95";
+		// TheToken Fund ethereum address
+		ethAddress = "0xBA3b826539161A4c3BF681752021847c25A2B46a"
+	    // TheToken Fund multisig address
+	    var multisig = "0x6E8Ec8a086B485940B435D18303b59b6C35048C1";
+	    // EtherionLab multisig address.
+	    var support = "0x9e8fAb94CADA52a584Ec6F10042c3Bb165E79114";
   }
 
 	var tokenContract;
